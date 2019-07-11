e1d19eb4c74efc4b575c589ae5c38c356a80156c
Andrey <geass1000@gmail.com>
2019-07-11 15:34:48 +0300

    feature(core): add `reexport` logic

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import * as Interceptorrs from './interceptors';
+import * as Decorators from './decorators';
+import * as Exceptions from './exceptions';
+import * as Modules from './modules';
+
+export {
+  Interceptorrs,
+  Decorators,
+  Exceptions,
+  Modules,
+};
---
+++ src/modules/index.ts
@@ -0,0 +1,11 @@
+import * as Cache from './cache';
+import * as Database from './database';
+import * as Logger from './logger';
+import * as Request from './request';
+
+export {
+  Cache,
+  Database,
+  Logger,
+  Request,
+};
